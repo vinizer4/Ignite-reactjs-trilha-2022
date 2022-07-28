455e1d67abf71bae12d66439fd53eafe8b5a8baf
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2022-07-28 00:17:46 -0300

    segunda aula

--- Fundamentos ReactJs/Project/01-fundamentos-reactjs/src/App.jsx
+++ Fundamentos ReactJs/Project/01-fundamentos-reactjs/src/App.jsx
@@ -1,19 +1,28 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
-import './styles.css'
+import styles from "./App.module.css";
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-      author="Diego Fernandes" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facere dolorum eveniet, autem iste reiciendis! Est expedita inventore nesciunt soluta ipsam porro eius, quas dolores nihil enim temporibus non esse."
-      />
-      <Post 
-      author="Gabriel Buzzi"
-      content="Um novo post muito legal"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Diego Fernandes" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facere dolorum eveniet, autem iste reiciendis! Est expedita inventore nesciunt soluta ipsam porro eius, quas dolores nihil enim temporibus non esse."
+          />
+          <Post 
+          author="Gabriel Buzzi"
+          content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
